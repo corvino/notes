c4549a377ec03afe70df59479960fe366068bbec
Nathan Corvino <nathan@corvino.com>
2025-11-15 22:40:40 -0800

    Another big update. Trying to move toward a cleaner orginization.

---
+++ skiing/nordicskilab-videojson.js
@@ -0,0 +1,240 @@
+module.exports = [
+  // Skate
+
+  {
+    name: "Learn to Skate Ski Program Rationale",
+    url: ""
+  },
+
+  // Learn to Skate Beginner Questions Answered
+
+  {
+    name:"Learn to Skate QA - 1 - The 5 Skate Skiing Techniques Explained",
+    url: ""
+  },
+  {
+    name:"Learn to Skate QA - 2 - Why is it Hard to Make a Skate Ski Work?",
+    url: ""
+  },
+  {
+    name:"Learn to Skate QA - 3 -The Skate Push Glide Explained",
+    url: ""
+  },
+  {
+    name:"Learn to Skate QA - 4 - How To Tell Offset And Two Skate Apart",
+    url: ""
+  },
+
+  // Learn to Skate Basic Skills
+
+  {
+    name: "Basic Skills for New Skate Skiers - 1 - Know Before You Go",
+    url: ""
+  },
+  {
+    name: "Basic Skills for New Skate Skiers - 2 - Basic Double Poling for Skate Skiers",
+    url: ""
+  },
+  {
+    name: "Basic Skills for New Skate Skiers - 3 - Box Drill – Off Snow",
+    url: ""
+  },
+  {
+    name: "Basic Skills for New Skate Skiers - 4 - Glide Prep Drills",
+    url: ""
+  },
+  {
+    name: "Basic Skills for New Skate Skiers - 5 - Push Prep Drills",
+    url: ""
+  },
+  {
+    name: "Basic Skills for New Skate Skiers - 6 - Box Drill – On Snow",
+    url: ""
+  },
+  {
+    name: "Basic Skills for New Skate Skiers - 7 - No Poles Skate Skiing",
+    url: ""
+  },
+  {
+    name: "Basic Skills for New Skate Skiers - 8 - Free Skate Tutorial",
+    url: ""
+  },
+
+  // Learn to Skate Uphill Skate 1 (Diagonal Skate)
+
+  {
+    name: "Uphill Skate 1 - 1 - Know Before You Go – Diagonal Skate",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 1 - 2 - Sliding V",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 1 - 3 - Tick-Tock U",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 1 - 4 - Active Ankles",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 1 - 5 - Moving Forward",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 1 - 6 - Consolidation",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 1 - 7 - Adding Poles – Diagonal Skate",
+    url: ""
+  },
+
+  // Learn to Skate Uphill Skate 2 (Offset)
+
+  {
+    name: "Uphill Skate 2 - 1 - Know Before You Go",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 2 - 2 - Ideal Stance",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 2 - 3 - Teapot Tap",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 2 - 4 - One Pole Offset",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 2 - 5 - Walking Offset",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 2 - 6 - Offset",
+    url: ""
+  },
+  {
+    name: "Uphill Skate 2 - 7 - Woodchopper Cheat",
+    url: ""
+  },
+
+  // Learn to Skate - Skating on the Flats (One Skate & Two Skate)
+
+  {
+    name: "Skating on the Flats - 1 - Know Before You Go",
+    url: ""
+  },
+  {
+    name: "Skating on the Flats - 2 - Flex",
+    url: ""
+  },
+  {
+    name: "Skating on the Flats - 3 - Learn One Skate, Part 1",
+    url: ""
+  },
+  {
+    name: "Skating on the Flats - 4 - Learn One Skate, Part 2",
+    url: ""
+  },
+  {
+    name: "Skating on the Flats - 5 - Learn Two Skate",
+    url: ""
+  },
+  {
+    name: "Skating on the Flats - 6 - Super Mini Skate Skiing",
+    url: ""
+  },
+
+  // Downhill Skills
+
+  {
+    name: "Downhill Skills - Skid Turn & Hockey Stop Tutorial",
+    url: ""
+  },
+  {
+    name: "Downhill Turns in Races",
+    url: ""
+  },
+
+  // CCC Downhill Skills & Progressions
+
+  {
+    name: "Downhill Skills - High Speed Downhill Turns",
+    url: ""
+  },
+  {
+    name: "Downhill Skills - CC Downhill Skills & Progression - 1 - Snowplow",
+    url: ""
+  },
+  {
+    name: "Downhill Skills - CC Downhill Skills & Progression - 2 - Snowplow Turn",
+    url: ""
+  },
+  {
+    name: "Downhill Skills - CC Downhill Skills & Progression - 3 - Stem Christie",
+    url: ""
+  },
+  {
+    name: "Downhill Skills - CC Downhill Skills & Progression - 4 - Step Turn on Flats",
+    url: ""
+  },
+  {
+    name: "Downhill Skills - CC Downhill Skills & Progression - 5 - Step Turn on Downhill",
+    url: ""
+  },
+  {
+    name: "Downhill Skills - CC Downhill Skills & Progression - 6 - Skid Turn Traverse",
+    url: ""
+  },
+  {
+    name: "Downhill Skills - CC Downhill Skills & Progression - 7 - Hockey Stop",
+    url: ""
+  },
+  {
+    name: "Downhill Skills - CC Downhill Skills & Progression - 8 - Skid Turn",
+    url: ""
+  },
+  {
+    name: "Downhill Skills - CC Downhill Skills & Progression - 9 - Skid to Step Turn",
+    url: ""
+  },
+
+  // Learn Step Turns – Flats and Downhills
+
+  {
+    name: "Learn Step Turns - 1 - Know Before You Go",
+    url: ""
+  },
+  {
+    name: "Learn Step Turns - 2 - Movement Breakdown",
+    url: ""
+  },
+  {
+    name: "Learn Step Turns - 3 - Add Glide",
+    url: ""
+  },
+  {
+    name: "Learn Step Turns - 4 - Poling",
+    url: ""
+  },
+  {
+    name: "Learn Step Turns - 5 - Various Drills (flats)",
+    url: ""
+  },
+  {
+    name: "Learn Step Turns - 6 - Downhill Stance",
+    url: ""
+  },
+  {
+    name: "Learn Step Turns - 7 - Downhill Stepping",
+    url: ""
+  },
+  {
+    name: "Learn Step Turns - 8 - Downhill Step Turns",
+    url: ""
+  }
+];
